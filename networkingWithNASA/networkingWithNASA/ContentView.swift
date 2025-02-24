f3d0efaab7c0995a0d8761a60c492939be76d41c
Joseph-Rhodes <josephlrhodes2021@gmail.com>
2025-02-24 10:56:39 -0500

    Creation of the project

---
+++ networkingWithNASA/networkingWithNASA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  networkingWithNASA
+//
+//  Created by Joseph Rhodes on 2/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ networkingWithNASA/networkingWithNASA/networkingWithNASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkingWithNASAApp.swift
+//  networkingWithNASA
+//
+//  Created by Joseph Rhodes on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct networkingWithNASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
